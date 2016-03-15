29c2723a21ae35c0147445808ba1787f1d6ba036
Just1689 <just1689@gmail.com>
2016-03-15 10:51:38 +0200

    Class

---
+++ java/mafia-game-server/src/main/java/com/mafia/server/Game.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mafia.server;
+
+/**
+ *
+ * @author Just1689
+ */
+public class Game {
+    private String key;
+    
+}
