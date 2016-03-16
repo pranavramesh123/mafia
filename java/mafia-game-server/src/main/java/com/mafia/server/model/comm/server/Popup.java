5dcc6b5c8b1a4efcc7f002aab15366b5d9863a17
Justin Tamblyn <Just1689@gmail.com>
2016-03-16 18:26:07 +0200

    Impl for interface for server messages

--- java/mafia-game-server/src/main/java/com/mafia/server/model/comm/server/Popup.java
+++ java/mafia-game-server/src/main/java/com/mafia/server/model/comm/server/Popup.java
@@ -9,6 +9,26 @@ package com.mafia.server.model.comm.server;
  *
  * @author Just1689
  */
-public class Popup {
+public class Popup implements ServerMessage{
+
+    @Override
+    public String getType() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setType(String type) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getEvent() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setEvent(String event) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
