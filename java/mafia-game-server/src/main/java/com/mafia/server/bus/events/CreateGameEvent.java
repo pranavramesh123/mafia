2d3e9de64d5c7ab5d54b3663c289f3813c13382c
Justin Tamblyn <Just1689@gmail.com>
2016-03-16 21:04:50 +0200

    Create game

--- java/mafia-game-server/src/main/java/com/mafia/server/bus/events/CreateGameEvent.java
+++ java/mafia-game-server/src/main/java/com/mafia/server/bus/events/CreateGameEvent.java
@@ -6,9 +6,8 @@
 package com.mafia.server.bus.events;
 
 import com.mafia.server.bus.ent.GameEvents;
-import com.mafia.server.io.MessageRouter;
+import com.mafia.server.bus.notify.GameNotify;
 import com.mafia.server.model.comm.client.CreateGame;
-import com.mafia.server.model.comm.server.GameState;
 import com.mafia.server.model.state.Game;
 
 /**
@@ -16,25 +15,24 @@ import com.mafia.server.model.state.Game;
  * @author Just1689
  */
 public class CreateGameEvent implements Runnable, Event {
-    
+
     private CreateGame data;
     private String createdBy;
-    
+
     @Override
     public void run() {
         //impl
         System.out.println(data.toString());
         Game game = GameEvents.create(data.getName(), data.getPassCode(), createdBy);
-        
-        GameState gameState = new GameState(game.getGameState().toString(), game.getKey());
-        MessageRouter.sendMessage(game, gameState);
+
+        GameNotify.sendGameState(game);
     }
-    
+
     @Override
     public void setData(Object obj, String sessionId) {
         this.data = (CreateGame) obj;
         this.createdBy = sessionId;
-        
+
     }
-    
+
 }
---
+++ java/mafia-game-server/src/main/java/com/mafia/server/bus/notify/GameNotify.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mafia.server.bus.notify;
+
+import com.mafia.server.io.MessageRouter;
+import com.mafia.server.model.comm.server.GameState;
+import com.mafia.server.model.state.Game;
+
+/**
+ *
+ * @author Just1689
+ */
+public class GameNotify {
+
+    public static void sendGameState(Game game) {
+        GameState gameState = new GameState(game.getGameState().toString(), game.getKey());
+        MessageRouter.sendMessage(game, gameState);
+        System.out.println("Sending game state.");
+
+    }
+
+}
