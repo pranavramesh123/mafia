f4b66b9ae4a59d15a593b5f069c91109acc18cf1
Just1689 <just1689@gmail.com>
2016-03-16 12:31:47 +0200

    Refactoring

--- java/mafia-game-server/src/main/java/com/mafia/server/model/state/Game.java
+++ java/mafia-game-server/src/main/java/com/mafia/server/model/state/Game.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mafia.server.state;
+package com.mafia.server.model.state;
 
 import java.util.concurrent.ConcurrentHashMap;
 import javax.websocket.Session;
