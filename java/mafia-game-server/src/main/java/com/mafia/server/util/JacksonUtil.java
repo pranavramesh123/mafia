a43cdc2665d9ec3b97b64149e7c7a431b003fabc
Justin Tamblyn <Just1689@gmail.com>
2016-03-16 18:08:14 +0200

    Bug fix

--- java/mafia-game-server/src/main/java/com/mafia/server/util/JacksonUtil.java
+++ java/mafia-game-server/src/main/java/com/mafia/server/util/JacksonUtil.java
@@ -6,25 +6,25 @@
 package com.mafia.server.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.istack.internal.logging.Logger;
 import java.io.IOException;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Just1689
  */
 public class JacksonUtil<T> {
-    
+
     public T stringToObject(String json, Class clazz) {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             T result = (T) objectMapper.readValue(json, clazz);
             return result;
         } catch (IOException ex) {
-            Logger.getLogger(this.getClass()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
         }
         return null;
     }
-    
+
 }
