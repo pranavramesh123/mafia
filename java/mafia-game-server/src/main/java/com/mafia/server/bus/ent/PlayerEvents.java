3275c6d69222c2d86f97eb014e4b554d0171e4f7
Justin Tamblyn <Just1689@gmail.com>
2016-03-16 21:25:30 +0200

    Cleanup

--- java/mafia-game-server/src/main/java/com/mafia/server/bus/ent/PlayerEvents.java
+++ java/mafia-game-server/src/main/java/com/mafia/server/bus/ent/PlayerEvents.java
@@ -5,6 +5,7 @@
  */
 package com.mafia.server.bus.ent;
 
+import com.mafia.server.bus.notify.GameNotify;
 import com.mafia.server.model.state.Game;
 import com.mafia.server.model.state.Player;
 import com.mafia.server.model.state.Repository;
@@ -15,16 +16,18 @@ import javax.websocket.Session;
  * @author Just1689
  */
 public class PlayerEvents {
-
+    
     public static void playerQuits(Session session) {
         Player player = Repository.getPlayerBySession(session);
         player.getGame().removePlayer(player);
 
         //Todo: notify?
     }
-
+    
     public static void joinGame(Player player, Game game) {
         player.setGame(game);
+        game.addPlayer(player);
+        GameNotify.sendGameState(game);
     }
-
+    
 }
