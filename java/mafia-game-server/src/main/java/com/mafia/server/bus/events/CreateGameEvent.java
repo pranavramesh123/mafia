0b46a3efa1a76448348e582f2d431495b6b96d9e
Justin Tamblyn <Just1689@gmail.com>
2016-03-16 20:53:21 +0200

    CreateGame

--- java/mafia-game-server/src/main/java/com/mafia/server/bus/events/CreateGameEvent.java
+++ java/mafia-game-server/src/main/java/com/mafia/server/bus/events/CreateGameEvent.java
@@ -6,30 +6,35 @@
 package com.mafia.server.bus.events;
 
 import com.mafia.server.bus.ent.GameEvents;
+import com.mafia.server.io.MessageRouter;
 import com.mafia.server.model.comm.client.CreateGame;
+import com.mafia.server.model.comm.server.GameState;
+import com.mafia.server.model.state.Game;
 
 /**
  *
  * @author Just1689
  */
 public class CreateGameEvent implements Runnable, Event {
-
+    
     private CreateGame data;
     private String createdBy;
-
+    
     @Override
     public void run() {
         //impl
         System.out.println(data.toString());
-        GameEvents.create(data.getName(), data.getPassCode(), createdBy);
-
+        Game game = GameEvents.create(data.getName(), data.getPassCode(), createdBy);
+        
+        GameState gameState = new GameState(game.getGameState().toString(), game.getKey());
+        MessageRouter.sendMessage(game, gameState);
     }
-
+    
     @Override
     public void setData(Object obj, String sessionId) {
         this.data = (CreateGame) obj;
         this.createdBy = sessionId;
-
+        
     }
-
+    
 }
---
+++ java/mafia-game-server/src/main/java/com/mafia/server/model/comm/server/GameState.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mafia.server.model.comm.server;
+
+/**
+ *
+ * @author Just1689
+ */
+public class GameState extends ServerMessage {
+
+    private String state;
+    private String key;
+
+    public GameState(String state, String key) {
+        this.state = state;
+        this.key = key;
+    }
+
+    /**
+     * @return the state
+     */
+    public String getState() {
+        return state;
+    }
+
+    /**
+     * @param state the state to set
+     */
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    /**
+     * @return the key
+     */
+    public String getKey() {
+        return key;
+    }
+
+    /**
+     * @param key the key to set
+     */
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+}
