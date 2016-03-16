9980bfd11c1149cd09d17e27dbb852114a38f3fe
Justin Tamblyn <Just1689@gmail.com>
2016-03-16 17:57:33 +0200

    Event interface

---
+++ java/mafia-game-server/src/main/java/com/mafia/server/bus/events/Event.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mafia.server.bus.events;
+
+/**
+ *
+ * @author Just1689
+ */
+public interface Event {
+
+    public void setData(Object obj);
+
+}
