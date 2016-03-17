c71fa5f93868aea5652719664bed0e4007dd854f
Justin Tamblyn <Just1689@gmail.com>
2016-03-17 20:00:26 +0200

    Cleanup

--- java/mafia-game-server/src/main/java/com/mafia/server/model/comm/server/GameState.java
+++ java/mafia-game-server/src/main/java/com/mafia/server/model/comm/server/GameState.java
@@ -11,40 +11,4 @@ package com.mafia.server.model.comm.server;
  */
 public class GameState extends ServerMessage {
 
-    private String state;
-    private String key;
-
-    public GameState(String state, String key) {
-        this.state = state;
-        this.key = key;
-    }
-
-    /**
-     * @return the state
-     */
-    public String getState() {
-        return state;
-    }
-
-    /**
-     * @param state the state to set
-     */
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    /**
-     * @return the key
-     */
-    public String getKey() {
-        return key;
-    }
-
-    /**
-     * @param key the key to set
-     */
-    public void setKey(String key) {
-        this.key = key;
-    }
-
 }
