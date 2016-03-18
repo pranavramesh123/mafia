de376142ca1d691de0593029304846688229647f
Justin Tamblyn <Just1689@gmail.com>
2016-03-18 23:04:52 +0200

    isDone impl for NightMurderActivity

--- java/mafia-game-server/src/main/java/com/mafia/server/model/acts/NightMurderActivity.java
+++ java/mafia-game-server/src/main/java/com/mafia/server/model/acts/NightMurderActivity.java
@@ -5,16 +5,21 @@
  */
 package com.mafia.server.model.acts;
 
+import com.mafia.server.bus.events.PlayerEvents;
 import static com.mafia.server.model.state.MafiaTypes.ACTIVITY_PARTICIPATION.GROUP;
 import com.mafia.server.model.state.Player;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  *
  * @author Just1689
  */
 public class NightMurderActivity extends Activity {
-    
+
     public NightMurderActivity(ArrayList<Player> players, boolean assignToPlayer) {
         super(100, GROUP, players);
         if (assignToPlayer) {
@@ -23,15 +28,46 @@ public class NightMurderActivity extends Activity {
             }
         }
     }
-    
+
     @Override
     public void vote(Player player, String vote) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
     @Override
     public boolean isDone() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (getPlayers().size() == getVotes().size()) {
+            if (getConcensusPercentage() == 100) {
+                Enumeration<String> elements = getVotes().elements();
+                String last=  null;
+                if (elements.hasMoreElements()) {
+                    last = elements.nextElement();
+                }
+                while (elements.hasMoreElements()) {
+                    String next = elements.nextElement();
+                    if (!next.equals(last)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
     }
-    
+
+    @Override
+    public void execute() {
+        ConcurrentHashMap<Player, String> votes = getVotes();
+        Set<Player> players = votes.keySet();
+        Iterator<Player> iterator = players.iterator();
+        iterator.hasNext();
+        Player player = iterator.next();
+        
+        String playerName = votes.get(player);
+        Player playerToKill = player.getGame().getPlayerByName(playerName);
+        
+        PlayerEvents.playerDies(playerToKill);
+        
+    }
+
 }
