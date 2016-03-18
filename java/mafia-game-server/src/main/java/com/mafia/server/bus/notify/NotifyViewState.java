93aca96129c02fa0bd5a44b6ebddf35daba32505
Justin Tamblyn <Just1689@gmail.com>
2016-03-18 23:19:08 +0200

    NotifyViewState: show what killers and nonkillers see at night

--- java/mafia-game-server/src/main/java/com/mafia/server/bus/notify/NotifyViewState.java
+++ java/mafia-game-server/src/main/java/com/mafia/server/bus/notify/NotifyViewState.java
@@ -9,6 +9,11 @@ import com.mafia.server.io.MessageRouter;
 import com.mafia.server.model.comm.server.ViewState;
 import com.mafia.server.model.state.Game;
 import com.mafia.server.model.state.MafiaTypes;
+import static com.mafia.server.model.state.MafiaTypes.ACTIVITY_PHASE.NIGHT;
+import static com.mafia.server.model.state.MafiaTypes.GAME_PHASE.ACTIVITY;
+import static com.mafia.server.model.state.MafiaTypes.GAME_PHASE.PREGAME;
+import com.mafia.server.model.state.Player;
+import java.util.ArrayList;
 
 /**
  *
@@ -17,7 +22,7 @@ import com.mafia.server.model.state.MafiaTypes;
 public class NotifyViewState {
 
     public static void nofity(Game game) {
-        if (game.getGamePhase().equals(MafiaTypes.GAME_PHASE.PREGAME)) {
+        if (game.getGamePhase().equals(PREGAME)) {
             ViewState viewState = new ViewState();
             viewState.setKey(game.getKey());
             viewState.setChat(true);
@@ -29,6 +34,40 @@ public class NotifyViewState {
             viewState.setInstructDiv(true);
             viewState.setInstructMessage("<strong>PREGAME:</strong> Type \"\\vote ready\" when ready to begin game");
             MessageRouter.sendMessage(game, viewState);
+        }
+
+        if (game.getGamePhase().equals(ACTIVITY)) {
+            if (game.getActivityPhase().equals(NIGHT)) {
+                ArrayList<Player> killers = game.getPlayersWithRole(MafiaTypes.PLAYER_ROLES.KILLER);
+                for (Player player : killers) {
+                    ViewState viewState = new ViewState();
+                    viewState.setKey(game.getKey());
+                    viewState.setChat(true);
+                    viewState.setChatInput(true);
+                    viewState.setCreateOrJoin(false);
+                    viewState.setLeaveGame(true);
+                    viewState.setVote(false);
+                    viewState.setReady(true);
+                    viewState.setInstructDiv(true);
+                    viewState.setInstructMessage("<strong>NIGHT:</strong> Type \"\\vote name\" of person to kill");
+                    MessageRouter.sendMessage(player, viewState);
+                }
+                ArrayList<Player> villagers = game.getPlayersWithoutRole(MafiaTypes.PLAYER_ROLES.KILLER);
+                for (Player player : villagers) {
+                    ViewState viewState = new ViewState();
+                    viewState.setKey(game.getKey());
+                    viewState.setChat(true);
+                    viewState.setChatInput(true);
+                    viewState.setCreateOrJoin(false);
+                    viewState.setLeaveGame(true);
+                    viewState.setVote(false);
+                    viewState.setReady(true);
+                    viewState.setInstructDiv(true);
+                    viewState.setInstructMessage("<strong>NIGHT:</strong> You are asleep");
+                    MessageRouter.sendMessage(player, viewState);
+                }
+                
+            }
         }
     }
 }
