9e26c11106b02da4de5f4213b444c8cb8cec53d9
Justin Tamblyn <Just1689@gmail.com>
2016-03-18 20:26:36 +0200

    Activity: core impl. for things that happen

---
+++ java/mafia-game-server/src/main/java/com/mafia/server/model/acts/Activity.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mafia.server.model.acts;
+
+import com.mafia.server.model.state.MafiaTypes.ACTIVITY_PARTICIPATION;
+import com.mafia.server.model.state.Player;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ *
+ * @author Just1689
+ */
+public abstract class Activity {
+
+    //Players by sessionId
+    private ConcurrentHashMap<String, Player> players;
+
+    //Votes of Player and name of player
+    private ConcurrentHashMap<Player, String> votes;
+
+    //Settings
+    private int concensusPercentage;
+    private ACTIVITY_PARTICIPATION participationType;
+
+    public abstract void vote(Player player, String vote);
+
+    public Activity(int concensusPercentage, ACTIVITY_PARTICIPATION participationType) {
+        this.players = new ConcurrentHashMap<>();
+        this.votes = new ConcurrentHashMap<>();
+        this.concensusPercentage = concensusPercentage;
+        this.participationType = participationType;
+    }
+
+    /**
+     * @return the players
+     */
+    public ConcurrentHashMap<String, Player> getPlayers() {
+        return players;
+    }
+
+    /**
+     * @param players the players to set
+     */
+    public void setPlayers(ConcurrentHashMap<String, Player> players) {
+        this.players = players;
+    }
+
+    /**
+     * @return the concensusPercentage
+     */
+    public int getConcensusPercentage() {
+        return concensusPercentage;
+    }
+
+    /**
+     * @param concensusPercentage the concensusPercentage to set
+     */
+    public void setConcensusPercentage(int concensusPercentage) {
+        this.concensusPercentage = concensusPercentage;
+    }
+
+    /**
+     * @return the participationType
+     */
+    public ACTIVITY_PARTICIPATION getParticipationType() {
+        return participationType;
+    }
+
+    /**
+     * @param participationType the participationType to set
+     */
+    public void setParticipationType(ACTIVITY_PARTICIPATION participationType) {
+        this.participationType = participationType;
+    }
+
+    /**
+     * @return the votes
+     */
+    public ConcurrentHashMap<Player, String> getVotes() {
+        return votes;
+    }
+
+    /**
+     * @param votes the votes to set
+     */
+    public void setVotes(ConcurrentHashMap<Player, String> votes) {
+        this.votes = votes;
+    }
+
+}
