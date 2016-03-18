e4d132defbd4fc78b867a150ac3f650f7cea0f65
Justin Tamblyn <Just1689@gmail.com>
2016-03-18 19:56:54 +0200

    PlayerList message for communicating the list of players to client

--- java/mafia-game-server/src/main/java/com/mafia/server/model/comm/server/PlayerList.java
+++ java/mafia-game-server/src/main/java/com/mafia/server/model/comm/server/PlayerList.java
@@ -5,10 +5,38 @@
  */
 package com.mafia.server.model.comm.server;
 
+import com.mafia.server.model.state.Player;
+import java.util.List;
+
 /**
  *
  * @author Just1689
  */
 public class PlayerList extends ServerMessage {
 
+    private String message;
+
+    public PlayerList(List<Player> players) {
+        StringBuilder out = new StringBuilder();
+        for (Player player : players) {
+            out.append("<span class=\"label label-default\">");
+            out.append(player.getName());
+            out.append("</span> ");
+        }
+        message = out.toString();
+    }
+
+    /**
+     * @return the message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * @param message the message to set
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
