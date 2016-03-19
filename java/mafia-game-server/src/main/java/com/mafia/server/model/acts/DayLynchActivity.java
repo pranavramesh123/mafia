d97ea600151dc58be7eb0ac4e35f780b83be9472
Justin Tamblyn <Just1689@gmail.com>
2016-03-19 22:21:41 +0200

    DayLynchActivity: vote impl

--- java/mafia-game-server/src/main/java/com/mafia/server/model/acts/DayLynchActivity.java
+++ java/mafia-game-server/src/main/java/com/mafia/server/model/acts/DayLynchActivity.java
@@ -5,9 +5,18 @@
  */
 package com.mafia.server.model.acts;
 
+import com.mafia.server.bus.events.ActivityCycler;
+import com.mafia.server.bus.notify.NotifyGame;
+import com.mafia.server.io.MessageRouter;
+import com.mafia.server.model.comm.server.ChatMessage;
+import com.mafia.server.model.comm.server.Messagebox;
 import com.mafia.server.model.state.Game;
 import com.mafia.server.model.state.MafiaTypes;
+import static com.mafia.server.model.state.MafiaTypes.PLAYER_ROLES.KILLER;
 import com.mafia.server.model.state.Player;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 /**
  *
@@ -16,7 +25,7 @@ import com.mafia.server.model.state.Player;
 public class DayLynchActivity extends Activity {
 
     public DayLynchActivity() {
-        super(100, MafiaTypes.ACTIVITY_PARTICIPATION.GROUP, null);
+        super(50, MafiaTypes.ACTIVITY_PARTICIPATION.GROUP, null);
     }
 
     @Override
@@ -27,16 +36,82 @@ public class DayLynchActivity extends Activity {
             return;
         }
 
+        if (vote == null) {
+            getVotes().remove(player);
+            MessageRouter.sendMessage(game, new ChatMessage(player.getName() + " removed their vote.<br />"));
+            return;
+        }
+
+        if (!vote.equals("abstain") && (getGame().getPlayerByName(vote) == null)) {
+            //Could not find X
+            MessageRouter.sendMessage(player, Messagebox.createMessageBoxError("Could not find player", vote));
+            return;
+        }
+
+        getVotes().put(player, vote);
+        MessageRouter.sendMessage(player.getGame().getPlayersAsList(), new ChatMessage(player.getName() + " voted for " + vote + "<br />"));
+        NotifyGame.sendPlayerList(player.getGame());
+        ActivityCycler.checkGame(player.getGame());
+
     }
 
     @Override
     public boolean isDone() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean enoughVotes = getVotes().size() >= getGame().getPlayersWhoAreAlive().size() / 2;
+        if (!enoughVotes) {
+            return false;
+        }
+
+        int needed = getGame().getPlayersWhoAreAlive().size();
+        System.out.println("Need " + needed + " votes to kill");
+        HashMap<String, Integer> totals = new HashMap<>();
+        for (Map.Entry<Player, String> entry : getVotes().entrySet()) {
+            Player player = entry.getKey();
+            String vote = entry.getValue();
+            Integer i = totals.get(vote);
+            if (i == null) {
+                totals.put(vote, 0);
+                i = totals.get(vote);
+            }
+            i++;
+            totals.put(vote, i);
+        }
+
+        for (Map.Entry<String, Integer> entry : totals.entrySet()) {
+            if (entry.getValue() >= needed) {
+                //Found!
+                return true;
+            }
+        }
+        return false;
+
     }
 
     @Override
     public void execute() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int needed = getGame().getPlayersWhoAreAlive().size() / 2;
+        HashMap<String, Integer> totals = new HashMap<>();
+        for (Map.Entry<Player, String> entry : getVotes().entrySet()) {
+            Player player = entry.getKey();
+            String vote = entry.getValue();
+            Integer i = totals.get(vote);
+            if (i == null) {
+                totals.put(vote, 0);
+                i = totals.get(vote);
+            }
+            i++;
+            totals.put(vote, i);
+        }
+
+        for (Map.Entry<String, Integer> entry : totals.entrySet()) {
+            if (entry.getValue() >= needed) {
+                //Found!
+                Player player = getGame().getPlayerByName(entry.getKey());
+                player.setAlive(false);
+                MessageRouter.sendMessage(getGame(), new ChatMessage(player.getName() + " has died<br />"));
+
+            }
+        }
     }
 
 }
