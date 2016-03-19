0ae03a848f9ee8249f3f35afd93dc3c352c51d7c
Justin Tamblyn <Just1689@gmail.com>
2016-03-19 19:46:20 +0200

    Impl for NightInvestigateActivity

--- java/mafia-game-server/src/main/java/com/mafia/server/model/acts/NightInvestigateActivity.java
+++ java/mafia-game-server/src/main/java/com/mafia/server/model/acts/NightInvestigateActivity.java
@@ -8,6 +8,7 @@ package com.mafia.server.model.acts;
 import com.mafia.server.bus.events.ActivityCycler;
 import com.mafia.server.bus.notify.NotifyGame;
 import com.mafia.server.io.MessageRouter;
+import com.mafia.server.model.comm.server.ChatMessage;
 import com.mafia.server.model.comm.server.Messagebox;
 import com.mafia.server.model.state.Game;
 import static com.mafia.server.model.state.MafiaTypes.ACTIVITY_PARTICIPATION.INDIVIDUAL;
@@ -18,7 +19,7 @@ import com.mafia.server.model.state.Player;
  * @author Just1689
  */
 public class NightInvestigateActivity extends Activity {
-
+    
     public NightInvestigateActivity(Player player, boolean assignToPlayer) {
         super(100, INDIVIDUAL);
         getPlayers().put(player.getSessionId(), player);
@@ -26,7 +27,7 @@ public class NightInvestigateActivity extends Activity {
             player.setActivity(this);
         }
     }
-
+    
     @Override
     public void vote(Player player, String vote) {
         Game game = player.getGame();
@@ -39,21 +40,24 @@ public class NightInvestigateActivity extends Activity {
             MessageRouter.sendMessage(player, Messagebox.createMessageBoxError("Error", "Player does not exist: " + vote));
             return;
         }
-
+        
         getVotes().put(player, vote);
         NotifyGame.sendPlayerList(player.getGame());
         ActivityCycler.checkGame(player.getGame());
-
+        
     }
-
+    
     @Override
     public boolean isDone() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return getVotes().size() == 1;
     }
-
+    
     @Override
     public void execute() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Player votePlayer = getVotedPlayer();
+        if (votePlayer != null) {
+            MessageRouter.sendMessage(getAPlayer(), new ChatMessage(votePlayer.getName() + " is a " + votePlayer.getRole().name()));
+        }
     }
-
+    
 }
