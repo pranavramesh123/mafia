066f7ee71e01af6456016adf83ec8bc4cb79d3e8
Justin Tamblyn <Just1689@gmail.com>
2016-03-19 20:37:29 +0200

    Added views for witches and investigators during night and dawn

--- java/mafia-game-server/src/main/java/com/mafia/server/bus/notify/NotifyViewState.java
+++ java/mafia-game-server/src/main/java/com/mafia/server/bus/notify/NotifyViewState.java
@@ -8,10 +8,14 @@ package com.mafia.server.bus.notify;
 import com.mafia.server.io.MessageRouter;
 import com.mafia.server.model.comm.server.ViewState;
 import com.mafia.server.model.state.Game;
-import com.mafia.server.model.state.MafiaTypes;
+import static com.mafia.server.model.state.MafiaTypes.ACTIVITY_PHASE.DAWN;
 import static com.mafia.server.model.state.MafiaTypes.ACTIVITY_PHASE.NIGHT;
 import static com.mafia.server.model.state.MafiaTypes.GAME_PHASE.ACTIVITY;
 import static com.mafia.server.model.state.MafiaTypes.GAME_PHASE.PREGAME;
+import static com.mafia.server.model.state.MafiaTypes.PLAYER_ROLES.CIVILIAN;
+import static com.mafia.server.model.state.MafiaTypes.PLAYER_ROLES.INVESTIGATOR;
+import static com.mafia.server.model.state.MafiaTypes.PLAYER_ROLES.KILLER;
+import static com.mafia.server.model.state.MafiaTypes.PLAYER_ROLES.WITCH_TYPE;
 import com.mafia.server.model.state.Player;
 import java.util.ArrayList;
 
@@ -38,7 +42,7 @@ public class NotifyViewState {
 
         if (game.getGamePhase().equals(ACTIVITY)) {
             if (game.getActivityPhase().equals(NIGHT)) {
-                ArrayList<Player> killers = game.getPlayersWithRole(MafiaTypes.PLAYER_ROLES.KILLER);
+                ArrayList<Player> killers = game.getPlayersWithRole(KILLER);
                 for (Player player : killers) {
                     ViewState viewState = new ViewState();
                     viewState.setKey(game.getKey());
@@ -52,7 +56,7 @@ public class NotifyViewState {
                     viewState.setInstructMessage("<strong>NIGHT:</strong> Type \"\\vote name\" of person to kill");
                     MessageRouter.sendMessage(player, viewState);
                 }
-                ArrayList<Player> villagers = game.getPlayersWithoutRole(MafiaTypes.PLAYER_ROLES.KILLER);
+                ArrayList<Player> villagers = game.getPlayersWithRole(CIVILIAN);
                 for (Player player : villagers) {
                     ViewState viewState = new ViewState();
                     viewState.setKey(game.getKey());
@@ -64,6 +68,92 @@ public class NotifyViewState {
                     viewState.setReady(true);
                     viewState.setInstructDiv(true);
                     viewState.setInstructMessage("<strong>NIGHT:</strong> You are asleep");
+                    MessageRouter.sendMessage(player, viewState);
+                }
+                ArrayList<Player> witches = game.getPlayersWithRole(WITCH_TYPE);
+                for (Player player : witches) {
+                    ViewState viewState = new ViewState();
+                    viewState.setKey(game.getKey());
+                    viewState.setChat(true);
+                    viewState.setChatInput(true);
+                    viewState.setCreateOrJoin(false);
+                    viewState.setLeaveGame(true);
+                    viewState.setVote(false);
+                    viewState.setReady(true);
+                    viewState.setInstructDiv(true);
+                    viewState.setInstructMessage("<strong>NIGHT:</strong> Kill someone");
+                    MessageRouter.sendMessage(player, viewState);
+                }
+                ArrayList<Player> investigators = game.getPlayersWithRole(INVESTIGATOR);
+                for (Player player : investigators) {
+                    ViewState viewState = new ViewState();
+                    viewState.setKey(game.getKey());
+                    viewState.setChat(true);
+                    viewState.setChatInput(true);
+                    viewState.setCreateOrJoin(false);
+                    viewState.setLeaveGame(true);
+                    viewState.setVote(false);
+                    viewState.setReady(true);
+                    viewState.setInstructDiv(true);
+                    viewState.setInstructMessage("<strong>NIGHT:</strong> Investigate someone");
+                    MessageRouter.sendMessage(player, viewState);
+                }
+
+            } else if (game.getActivityPhase().equals(DAWN)) {
+                ArrayList<Player> killers = game.getPlayersWithRole(KILLER);
+                for (Player player : killers) {
+                    ViewState viewState = new ViewState();
+                    viewState.setKey(game.getKey());
+                    viewState.setChat(true);
+                    viewState.setChatInput(true);
+                    viewState.setCreateOrJoin(false);
+                    viewState.setLeaveGame(true);
+                    viewState.setVote(false);
+                    viewState.setReady(true);
+                    viewState.setInstructDiv(true);
+                    viewState.setInstructMessage("<strong>DAWN:</strong> You are asleep");
+                    MessageRouter.sendMessage(player, viewState);
+                }
+                ArrayList<Player> villagers = game.getPlayersWithRole(CIVILIAN);
+                for (Player player : villagers) {
+                    ViewState viewState = new ViewState();
+                    viewState.setKey(game.getKey());
+                    viewState.setChat(true);
+                    viewState.setChatInput(true);
+                    viewState.setCreateOrJoin(false);
+                    viewState.setLeaveGame(true);
+                    viewState.setVote(false);
+                    viewState.setReady(true);
+                    viewState.setInstructDiv(true);
+                    viewState.setInstructMessage("<strong>DAWN:</strong> You are asleep");
+                    MessageRouter.sendMessage(player, viewState);
+                }
+                ArrayList<Player> witches = game.getPlayersWithRole(WITCH_TYPE);
+                for (Player player : witches) {
+                    ViewState viewState = new ViewState();
+                    viewState.setKey(game.getKey());
+                    viewState.setChat(true);
+                    viewState.setChatInput(true);
+                    viewState.setCreateOrJoin(false);
+                    viewState.setLeaveGame(true);
+                    viewState.setVote(false);
+                    viewState.setReady(true);
+                    viewState.setInstructDiv(true);
+                    viewState.setInstructMessage("<strong>DAWN:</strong> Save someone");
+                    MessageRouter.sendMessage(player, viewState);
+                }
+                ArrayList<Player> investigators = game.getPlayersWithRole(INVESTIGATOR);
+                for (Player player : investigators) {
+                    ViewState viewState = new ViewState();
+                    viewState.setKey(game.getKey());
+                    viewState.setChat(true);
+                    viewState.setChatInput(true);
+                    viewState.setCreateOrJoin(false);
+                    viewState.setLeaveGame(true);
+                    viewState.setVote(false);
+                    viewState.setReady(true);
+                    viewState.setInstructDiv(true);
+                    viewState.setInstructMessage("<strong>DAWN:</strong> You are asleep");
                     MessageRouter.sendMessage(player, viewState);
                 }
 
